32c5bb55604027f225c7ac28d180c6e4c4f6eaad
pinky83 <pinky8388@gmail.com>
2017-03-21 12:38:52 +0200

    all classes moved to  modules

---
+++ test/src/test/java/ru/javaops/masterjava/xml/util/JaxbParserTest.java
@@ -0,0 +1,46 @@
+package ru.javaops.masterjava.xml.util;
+
+import com.google.common.io.Resources;
+import org.junit.Test;
+import xml.schema.CityType;
+import xml.schema.ObjectFactory;
+import xml.schema.Payload;
+import xml.util.JaxbParser;
+import xml.util.Schemas;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.namespace.QName;
+
+/**
+ * gkislin
+ * 23.09.2016
+ */
+public class JaxbParserTest {
+    private static final JaxbParser JAXB_PARSER = new JaxbParser(ObjectFactory.class);
+
+    static {
+        JAXB_PARSER.setSchema(Schemas.ofClasspath("payload.xsd"));
+    }
+
+    @Test
+    public void testPayload() throws Exception {
+//        JaxbParserTest.class.getResourceAsStream("/city.xml")
+        Payload payload = JAXB_PARSER.unmarshal(
+                Resources.getResource("payload.xml").openStream());
+        String strPayload = JAXB_PARSER.marshal(payload);
+        JAXB_PARSER.validate(strPayload);
+        System.out.println(strPayload);
+    }
+
+    @Test
+    public void testCity() throws Exception {
+        JAXBElement<CityType> cityElement = JAXB_PARSER.unmarshal(
+                Resources.getResource("city.xml").openStream());
+        CityType city = cityElement.getValue();
+        JAXBElement<CityType> cityElement2 =
+                new JAXBElement<>(new QName("http://javaops.ru", "City"), CityType.class, city);
+        String strCity = JAXB_PARSER.marshal(cityElement2);
+        JAXB_PARSER.validate(strCity);
+        System.out.println(strCity);
+    }
+}

452304f4dddb1a58f918a3510dc89806d1258762
pinky83 <pinky8388@gmail.com>
2017-04-02 12:45:54 +0300

    transfer code from tests to userExport class

--- web/export/src/main/java/ru/javaops/masterjava/export/UserExport.java
+++ web/export/src/main/java/ru/javaops/masterjava/export/UserExport.java
@@ -1,5 +1,7 @@
 package ru.javaops.masterjava.export;
 
+import ru.javaops.masterjava.persist.DBIProvider;
+import ru.javaops.masterjava.persist.dao.UserDao;
 import ru.javaops.masterjava.persist.model.User;
 import ru.javaops.masterjava.persist.model.UserFlag;
 import ru.javaops.masterjava.xml.util.StaxStreamProcessor;
@@ -7,8 +9,10 @@ import ru.javaops.masterjava.xml.util.StaxStreamProcessor;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.XMLEvent;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.DriverManager;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * gkislin
@@ -16,17 +20,71 @@ import java.util.List;
  */
 public class UserExport {
 
-    public List<User> process(final InputStream is) throws XMLStreamException {
-        final StaxStreamProcessor processor = new StaxStreamProcessor(is);
-        List<User> users = new ArrayList<>();
+    static {
+        DBIProvider.init(() -> {
+            try {
+                Class.forName("org.postgresql.Driver");
+            } catch (ClassNotFoundException e) {
+                throw new IllegalStateException("PostgreSQL driver not found", e);
+            }
+            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/masterjava", "user", "password");
+        });
 
+    }
+
+    private final UserDao DAO = DBIProvider.getDao(UserDao.class);
+    //email validation
+    private final String regex = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+
+    private List<User> users = new ArrayList<>();
+    private static Map<Integer, String> invalidUsers = new HashMap<>();
+    private final Pattern pattern = Pattern.compile(regex);
+
+    public List<User> process(final InputStream is) throws XMLStreamException {
+
+        final StaxStreamProcessor processor = new StaxStreamProcessor(is);
+
+        int count = 0; //counter of invalid users
         while (processor.doUntil(XMLEvent.START_ELEMENT, "User")) {
-            final String email = processor.getAttribute("email");
-            final UserFlag flag = UserFlag.valueOf(processor.getAttribute("flag"));
-            final String fullName = processor.getReader().getElementText();
-            final User user = new User(fullName, email, flag);
+            count++;
+            User user;
+            String fullName = null;
+            Optional<String> email = Optional.ofNullable(processor.getAttribute("email"));
+            try {
+                Matcher matcher = pattern.matcher(email.orElseThrow(IllegalArgumentException::new));
+                if(!matcher.matches()){
+                    fullName = processor.getReader().getElementText();
+                    throw new Exception();
+                }
+                UserFlag flag = UserFlag.valueOf(processor.getAttribute("flag"));
+                fullName = processor.getReader().getElementText();
+                if (fullName.equals("")) throw new Exception();
+                user = new User(fullName, email.orElse("no mail"), flag);
+            }catch (Exception e) {
+                if(!email.isPresent()){
+                    try{
+                        fullName = processor.getReader().getElementText();
+                    }catch (Exception ex) {invalidUsers.put(count, "user " + count + " - empty name and email");
+                        continue;}
+                }
+                invalidUsers.put(count,"user " + count + " " + fullName + " " + email.orElse("no email"));
+                continue;
+            }
             users.add(user);
         }
+
+        DAO.clean();
+        for (User u : users) {
+            try {
+                DAO.insert(u);
+            } catch (Exception e) {
+                invalidUsers.put(count, u.getFullName() + " " + u.getEmail());
+            }
+        }
+
+        System.out.format("Total : %d, Invalid : %d%n", count, invalidUsers.size());
+        invalidUsers.values().forEach(System.out::println);
+
         return users;
     }
 }
